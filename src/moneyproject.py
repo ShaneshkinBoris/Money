69d46cdddf06f82a2dd8d4b51bf65d111a522458
BorisShaneshkin <shaneshkin@gmail.com>
2010-11-27 16:49:36 +0300

    currency fail

--- src/moneyproject.py
+++ src/moneyproject.py
@@ -1,8 +1,9 @@
-import Dollar
+
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
-from Dollar import Dollar
-dollar = Dollar(0)
+from Dollar import Money
+dollar = Money.dollar(1)
 
+print dollar.__dict__
 if __name__ == "__main__":
     print "Hello World"
